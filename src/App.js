29f2e6a7da08e82e96b54415584398d9fd7f383a
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-05-15 13:12:56 -0300

    telainicial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Component, useState} from "react";
+import Page1 from "./componentes/pag1"
+function App() {
+  return (
+    <>
+        <Page1/>
+    </>  
+  );
+}
+
+export default App;
---
+++ src/componentes/pag1/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Page1 (props){
+    return (
+        <>
+        <div className="logo">
+          <img src="assets/img/logo.png" alt=""/>
+          <h1>
+            ZapRecall
+          </h1>
+        </div>
+        <button className="start">
+          <h2> Iniciar Recall! </h2>
+        </button> 
+        </>
+    );
+}
