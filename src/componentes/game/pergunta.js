c344702396a0cf046c0ffd0991fc80b61f4aebe1
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-05-16 06:03:47 -0300

    perguntaokfaltaresultado

--- src/componentes/game/pergunta.js
+++ src/componentes/game/pergunta.js
@@ -9,7 +9,7 @@ export default function Pergunta(props) {
   const [flipped, setFlipped] = React.useState(false);
   const [resposta, setRespostas] = React.useState(false);
   const [minimenu, setMinimenu] = React.useState(false);
-
+  
   let text;
   let modo;
   let img;
@@ -23,6 +23,7 @@ export default function Pergunta(props) {
     setTexto(true);
     setFlipped(false);
   }
+
   if(texto){
     text = "pergunta";
     modo = "front";
@@ -33,6 +34,7 @@ export default function Pergunta(props) {
     n = null;
     img = setinha();
   }
+
   function setinha(){
     return (
       <img src="assets/img/setinha.png" alt="" onClick={exibirrespostas}/>
@@ -43,25 +45,44 @@ export default function Pergunta(props) {
       <img src="assets/img/play.png" alt="" onClick={virarPergunta}/>
     );
   }
+
   function exibirrespostas(){
     setMinimenu(true);
   }
+
   if(minimenu){
     img = null;
     text = R;
     botoes = (
       <div className='menuBotoes'>
-        <div className='nao'>
+        <div className='nao' onClick={riscaNao}>
           <h5>Não lembrei</h5>
         </div>
-        <div className='quase'>
+        <div className='quase' onClick={riscaQuase}>
           <h5>Quase não lembrei</h5>
         </div>
-        <div className='sim'>
+        <div className='sim' onClick={riscaSim}>
           <h5>Zap!</h5>
         </div>
       </div>);  
   }
+
+  function riscaSim(){
+    setTexto(true);
+    setFlipped(false);
+    botoes = (""); 
+  }
+  function riscaQuase(){
+    setTexto(true);
+    setFlipped(false);
+    botoes = (""); 
+  }
+  function riscaNao(){
+    setTexto(true);
+    setFlipped(false);
+    botoes = (""); 
+  }
+
   return (
     <li className = {modo}>
       <h3> {text} {n}</h3>{img}{botoes}
--- src/componentes/game/perguntasData.js
+++ src/componentes/game/perguntasData.js
@@ -7,39 +7,39 @@ export default function getPerguntasData() {
     },
     {
         "n": " 2",
-        "Q": "Oque é JSX2?",
-        "R": "Uma extensão de linguagem do JavaScript"
+        "Q": "O React é __",
+        "R": "uma biblioteca JavaScript para construção de interfaces"
     },
     {
         "n": " 3",
-        "Q": "Oque é JSX3?",
-        "R": "Uma extensão de linguagem do JavaScript"
+        "Q": "Componentes devem iniciar com __ ",
+        "R": "letra maiúscula"
     },
     {
         "text": "Pergunta",
         "n": " 4",
-        "Q": "Oque é JSX4?",
+        "Q": "Podemos colocar __ dentro do JSX",
         "R": "Uma extensão de linguagem do JavaScript"
     },
     {
         "n": " 5",
-        "Q": "Oque é JSX5?",
-        "R": "Uma extensão de linguagem do JavaScript"
+        "Q": "O ReactDOM nos ajuda __",
+        "R": "interagindo com a DOM para colocar componentes React na mesma"
     },
     {
         "n": " 6",
-        "Q": "Oque é JSX?",
-        "R": "Uma extensão de linguagem do JavaScript"
+        "Q": "Usamos o npm para __",
+        "R": "gerenciar os pacotes necessários e suas dependências"
     },
     {
         "n": " 7",
-        "Q": "Oque é JSX?",
-        "R": "Uma extensão de linguagem do JavaScript"
+        "Q": "Usamos props para __ ",
+        "R": "passar diferentes informações para componentes "
     },
     {
         "n": " 8",
-        "Q": "Oque é JSX?",
-        "R": "Uma extensão de linguagem do JavaScript"
+        "Q": "Usamos estado (state) para __",
+        "R": "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
     }
     ];
   }
